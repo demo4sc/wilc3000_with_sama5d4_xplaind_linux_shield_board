7511fc281efd1d16ec0a651f2a7302298c3a9b36
Chris Park <chris.park@atmel.com>
2016-02-19 14:46:30 +0900

    First upload WFA TEST binary

--- demo_linux_nandflash.sh
+++ demo_linux_nandflash.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sam-ba /dev/ttyACM0 at91sama5d4x-ek demo_linux_nandflash.tcl 2>&1 | tee logfile.log
+sam-ba /dev/ttyACM0 at91sama5d4x-ek demo_linux_nandflash.tcl > logfile.log 2>&1
+cat logfile.log
